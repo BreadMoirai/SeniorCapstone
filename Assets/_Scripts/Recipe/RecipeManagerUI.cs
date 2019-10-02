70c3686f55877a60ddae29b3d778d1a5c6cd56bc
Robert <rcastillods@gmail.com>
2019-10-01 20:08:12 -0700

    Worked on overflow reviews

--- Assets/_Scripts/Recipe/RecipeManagerUI.cs
+++ Assets/_Scripts/Recipe/RecipeManagerUI.cs
@@ -245,6 +245,7 @@ public class RecipeManagerUI : MonoBehaviour
     public void ShowMoreReviews()
     {
         canvas.SetActive(false);
+        ReviewManagerUI.Instance.InitReviewUI(thisRecipe);
         ReviewManagerUI.Instance.Enable();
     }
 
